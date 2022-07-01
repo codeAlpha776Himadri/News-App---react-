290d06aa4e64ba37422b92de6e2c099780b7bb8b
Himadri <hck364864@gmail.com>
2022-07-02 03:32:11 +0530

    added fetch

--- src/News.js
+++ src/News.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import './News.css'
 
-export class NewsComponent extends Component {
+export class News extends Component {
+  constructor() {
+    super() ; 
+    console.log("constructor ran ") ; 
+    this.state = {
+      articles : [] , 
+      loading : true ,
+      status :  'fetching'
+    }
+  }
+
+  componentDidMount = async () => {
+    const url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=686a4fba5eab4634864546caad2cf1a7' ;
+    const response = await fetch(url) ; 
+    const data = await response.json() ; 
+    const newsArticles = data.articles ; 
+    this.setState({
+      articles : newsArticles , 
+      loading : false , 
+      status : 'success'
+    })
+    console.log('destructed' , this.state.articles.length) ;
+  }
+
   render() {
+    
     return (
-      <div>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      
+      <div className='News'>
+        <div className="heading">
+          { (this.state.loading === true) ?  <div class="spinner-border text-success " role="status"></div> : <div><h2>Welcome to News World</h2></div> }
+        </div>
+          
+        <div className="row  px-5  my-3">
+          {
+            this.state.articles.map(({ title , description , urlToImage , url}) => {
+              if (title === null || description === null || urlToImage === null) return ; 
+              return  <div className="col-md-4  my-3">
+                        <NewsItem  
+                            title={title} 
+                            description = {description} 
+                            urlImg={urlToImage}
+                            url={url}
+                        />
+                      </div>
+            })
+          }
+        </div>
+
       </div>
     )
   }
 }
 
-export default NewsComponent
+export default News
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import './NewsItem.css'
 
 export class NewsItem extends Component {
   render() {
+
+    let {title , description , urlImg , url } = this.props ; 
+
     return (
-      <div>NewsItem</div>
+      <div>
+        <div className="card " style={{width: '18rem'}}>
+            <img src={urlImg} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href={url}  target='_blank' rel='norefferer' className="btn btn-sm btn-success">Read more...</a>
+            </div>
+        </div>
+      </div>
     )
   }
 }
